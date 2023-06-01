012fb711bc9895c571b5f80df5affc54a3b902f6
Shubham-Shrivastav <shuboom@969@gmail.com>
2023-06-01 11:29:28 +0530

    changed requested

--- src/Context/DataContext.js
+++ src/Context/DataContext.js
@@ -3,18 +3,26 @@ import React, { createContext, useState } from 'react';
 const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
-    const [tableData, setTableData] = useState([]);
-    
-    // make a function to add directly to the table array
-    // formData object pass as an argument to the function
-  
+  const [tableData, setTableData] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    gender: '',
+    password: ''
+  });
+
+  // make a function to add directly to the table array
+  // formData object pass as an argument to the function
+
   const addFormData = (formData) => {
     setTableData([...tableData, formData]);
   };
 
   const dataContextValue = {
     tableData,
-    addFormData
+    addFormData,
+    formData,
+    setFormData
   };
 
   return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,21 +3,12 @@ import { Link } from 'react-router-dom';
 import { DataContext } from '../Context/DataContext';
 
 const Form = () => {
-  const { addFormData } = useContext(DataContext);
+  const { addFormData, formData,
+    setFormData } = useContext(DataContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    const formData = {
-      name: event.target.name.value,
-      email: event.target.email.value,
-      gender: event.target.gender.value,
-      password: event.target.password.value
-    };
-
     addFormData(formData);
-
-    event.target.reset();
   };
 
   return (
@@ -25,24 +16,48 @@ const Form = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Name:</label>
-          <input type="text" name="name" required />
+          <input value={formData.name}
+            onChange={event => setFormData({
+              ...formData, name: event.target.value
+            })
+            }
+            type="text"
+            name="name"
+            required />
         </div>
+
         <div>
           <label>Email:</label>
-          <input type="email" name="email" required />
+          <input value={formData.email}
+            onChange={(event) =>
+              setFormData({ ...formData, email: event.target.value })
+            }
+            type="email"
+            name="email"
+            required />
         </div>
+
         <div>
           <label>Gender:</label>
-          <select name="gender" required>
+          <select
+            value={formData.gender}
+            onChange={handleChange}
+            name="gender"
+            required
+          >
             <option value="">Other</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
         </div>
+
         <div>
           <label>Password:</label>
-          <input type="password" name="password" required />
+          <input value={formData.password}
+            onChange={(event) =>
+              setFormData({ ...formData, password: event.target.value })} type="password" name="password" required />
         </div>
+
         <button type="submit">Submit</button>
       </form>
       <Link to="/formdata">View Form Data</Link>
